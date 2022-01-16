bf1a5d84b1572172450d20d9b694b512eeb60b6d
b1m4s3n4 <64942799+bimasena321@users.noreply.github.com>
2022-01-16 14:31:10 +0700

    halo
    program sederhana

---
+++ runbima.java
@@ -0,0 +1,14 @@
+package tugas;
+import java.util.*;
+public class runbima{
+  public static void main(String [] args){
+   Runtime  app = Runtime.getRuntime();
+    try{
+      //ini masukan nama aplikasi di bagian app.exec ("nama aplikasi");
+      app.exec("notepad");
+      app.exec("calc");
+    }
+    catch(Exception ex){
+      System.outprintln("Error: "+ex.toString());
+  }
+}
